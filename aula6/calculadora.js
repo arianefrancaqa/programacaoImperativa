c6308d15d7b836eb3df6e770f236a0127289240e
Ariane França <ariane.franca@uberall.com>
2022-05-11 22:55:23 -0300

    removendo comentarios e formatando

--- aula6/calculadora.js
+++ aula6/calculadora.js
@@ -2,49 +2,32 @@ var readlineSync = require("readline-sync");
 
 //Adição
 function adicao(num1, num2) {
-  let resultado = num1 + num2;
-
-  //   console.log(`
-  //     O Resultado da adição é ${resultado}
-  //     `);
-  return resultado;
+    return num1 + num2;
 }
 
 //Subtração
 function subtracao(num1, num2) {
-  let resultado = num1 - num2;
-
-  //   console.log(`
-  //     O Resultado da subtração é ${resultado}
-  //     `);
-
-  return resultado;
+    return num1 - num2;
 }
 
 //Multiplicação
 function multiplicacao(num1 = 0, num2 = 0) {
-  let resultado = num1 * num2;
-
-  //   console.log(`
-  //     O Resultado da multiplicação é ${resultado}
-  //     `);
-
-  return resultado;
+    return num1 * num2;
 }
 
 //Divisão
 function divisao(num1, num2) {
-  return num1 / num2;
+    return num1 / num2;
 }
 
 //Numero ao quadrado
 function quadradoDoNumero(num1) {
-  return multiplicacao(num1, num1);
+    return multiplicacao(num1, num1);
 }
 
 //Calcula porcentagem
 function calculaPorcentagem(num1, num2) {
-  return num1 * (num2 / 100);
+    return num1 * (num2 / 100);
 }
 
 console.log("-------------- Teste de Operações / Calculadora --------------");
@@ -65,48 +48,48 @@ Para saber uma porcentagem digite 7;
 `);
 
 switch (operacao) {
-  case "1":
-    console.log(`
-    O resultado da adição ${num1} com ${num2} é ${adicao(num1, num2)}
-  `);
-    break;
+    case "1":
+        console.log(`
+            O resultado da adição ${num1} com ${num2} é ${adicao(num1, num2)}
+        `);
+        break;
 
-  case "2":
-    console.log(`
-    O resultado da subtração ${num1} com ${num2} é ${subtracao(num1, num2)}
-    `);
-    break;
+    case "2":
+        console.log(`
+            O resultado da subtração ${num1} com ${num2} é ${subtracao(num1, num2)}
+        `);
+        break;
 
-  case "3":
-    console.log(`
-O resultado da multiplicação ${num1} com ${num2} é ${multiplicacao(num1, num2)}
-`);
-    break;
+    case "3":
+        console.log(`
+            O resultado da multiplicação ${num1} com ${num2} é ${multiplicacao(num1, num2)}
+        `);
+        break;
 
-  case "4":
-    console.log(`
-O resultado da divisão ${num1} com ${num2} é ${divisao(num1, num2)}
-`);
-    break;
+    case "4":
+        console.log(`
+            O resultado da divisão ${num1} com ${num2} é ${divisao(num1, num2)}
+        `);
+        break;
 
-  case "5":
-    console.log(`
-O resultado da divisão de ${num2} por 0 é ${divisao(0, num2)}
-`);
-    break;
+    case "5":
+        console.log(`
+            O resultado da divisão de ${num2} por 0 é ${divisao(0, num2)}
+        `);
+        break;
 
-  case "6":
-    console.log(`
-O resultado do quadrado de ${num1} com ${num2} é ${quadradoDoNumero(num1)}
-`);
-    break;
+    case "6":
+        console.log(`
+            O resultado do quadrado de ${num1} com ${num2} é ${quadradoDoNumero(num1)}
+        `);
+        break;
 
-  case "7":
-    console.log(`
-${num2}% de ${num1} é ${calculaPorcentagem(num1, num2)}
-`);
-    break;
+    case "7":
+        console.log(`
+            ${num2}% de ${num1} é ${calculaPorcentagem(num1, num2)}
+        `);
+        break;
 
-  default:
-    console.log("Erro operação não reconhecida.");
+    default:
+        console.log("Erro operação não reconhecida.");
 }
